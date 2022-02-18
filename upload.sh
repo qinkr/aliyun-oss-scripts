5ada63265abc8181b247416f4a831c1cd1a53be8
qinkr <89576692+qinkr@users.noreply.github.com>
2022-02-18 11:21:17 +0800

    feat: update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,46 +1,21 @@
-#!/bin/sh
-set -e
+# aliyun-oss-scripts
+Simple aliyun oss shell scripts
 
+
+## How to use
+
+```shell
 ### aliyun oss config ###
 endpoint=""
 bucket=""
 access_key_id=""
 access_key_secret=""
-host=$bucket.$endpoint
+```
 
-error_exit ()
-{
-    echo "Error: $1"
-    exit 1
-}
+```shell
+sh ./upload.sh file
+# upload file to https://bucket.endpoint/file
 
-source=$1
-[ ! -f ${source} ] && error_exit "${source} is not existed"
-file_name=`basename ${source}`
-
-dest=${2:-'/'}
-file_dir=`dirname "${dest}/${file_name}"`
-[[ ${file_dir} == '/' || ${file_dir} == '.' ]] && file_dir=''
-
-resource="/${bucket}${file_dir}/${file_name}"
-content_type=`file --mime --brief ${source} |awk -F ";" '{print $1}'`
-date_value="`TZ=GMT env LANG=en_US.UTF-8 date +'%a, %d %b %Y %H:%M:%S GMT'`"
-if [[ `uname -a` =~ 'Darwin' ]]; then
-    # remove newline in macOS
-    string_to_sign="PUT\n\n${content_type}\n${date_value}\n${resource}\c"
-    signature=`echo ${string_to_sign} | openssl sha1 -hmac ${access_key_secret} -binary | base64`
-else
-    string_to_sign="PUT\n\n${content_type}\n${date_value}\n${resource}"
-    signature=`echo -en ${string_to_sign} | openssl sha1 -hmac ${access_key_secret} -binary | base64`
-fi
-
-url="https://${host}${file_dir}/${file_name}"
-
-curl --silent --include --output /dev/null -q -X PUT -T "${source}" \
-    -H "Host: ${host}" \
-    -H "Date: ${date_value}" \
-    -H "Content-Type: ${content_type}" \
-    -H "Authorization: OSS ${access_key_id}:${signature}" \
-    ${url}
-
-echo "upload ${source} to ${url}"
+sh ./upload.sh file /path
+# upload file to https://bucket.endpoint/path/file
+```
